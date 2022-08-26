25625ed17b8c0980e0ef50579d8458e13052ca45
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-08-26 21:49:53 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+function sumFunc(a, b) {
+  return a + b;
+}
+
+function subFunc(a, b) {
+  return a - b;
+}
+
+function mulFunc(a, b) {
+  return a * b;
+}
+
+sumFunc(2, 4);
+subFunc(2, 4);
+mulFunc(2, 4);
